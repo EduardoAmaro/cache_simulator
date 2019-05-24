add7c1504aa9d1e3b8a7eabb09c27ac0ed5e4ad9
EduardoAmaro <dudaamarorosa@gmail.com>
2019-05-23 22:16:34 -0300

    Adicionada criação da cache e dos blocos (lista de listas)

--- src/cache_simulator/Cache.java
+++ src/cache_simulator/Cache.java
@@ -4,12 +4,29 @@ import java.util.ArrayList;
 
 public class Cache {
     Integer noffset, nindice, ntag;
-    
-    ArrayList<ArrayList> conjuntos;
+    ArrayList<ArrayList<Bloco>> conjuntos;
 
     public Cache(Integer nsets, Integer assoc, Integer bsize) {
         this.noffset = (int) (Math.log(bsize)/Math.log(2));
         this.nindice = (int) (Math.log(nsets)/Math.log(2));
         this.ntag = 32 - this.noffset - this.nindice;
+        this.conjuntos = new ArrayList<>(assoc);
+        
+        for(int i=0;i<assoc;i++){
+            ArrayList<Bloco> blocos = new ArrayList<>();
+            for(int j=0;j<nsets;j++){    
+                Bloco bloco = new Bloco();
+                blocos.add(bloco);
+            }
+            this.conjuntos.add(blocos);
+        }
+        
+        for(int i=0;i<assoc;i++){
+            System.out.print("[");
+            for(int j=0;j<nsets;j++){    
+                System.out.print(j + ",");
+            }
+            System.out.print("]\n");
+        }
     }
 }
--- src/cache_simulator/Cache_simulator.java
+++ src/cache_simulator/Cache_simulator.java
@@ -18,6 +18,7 @@ public class Cache_simulator {
         Integer nsets = Integer.parseInt(config.split(":")[0]);
         Integer bsize = Integer.parseInt(config.split(":")[1]);
         Integer assoc = Integer.parseInt(config.split(":")[2]);
+        
         System.out.println(nsets);
         System.out.println(bsize);
         System.out.println(assoc);
@@ -49,7 +50,8 @@ public class Cache_simulator {
             System.out.println(enderecos.get(i));
         }
         System.out.println(enderecos.size());
-
+        
+        Cache cache = new Cache(nsets,assoc,bsize);
     }
 
     public static String preencheBinario(String endereco) {
@@ -59,7 +61,11 @@ public class Cache_simulator {
         return endereco;
     }
 
-//cache_simulator<nsets_L1>:<bsize_L1>:<assoc_L1> arquivo_de_entrada
-//java cache_simulator.java 4:10:1 arqBinario1.dat
-//java -jar cache_simulator.jar 4:10:1 arqBinario1.dat
+/*
+    cache_simulator<nsets_L1>:<bsize_L1>:<assoc_L1> arquivo_de_entrada
+    
+    rodar na pasta src/
+    javac cache_simulator/Cache_simulator.java
+    java cache_simulator.Cache_simulator 256:64:4 arqBinario1.dat
+*/
 }
