d34d01c3496824ab31b3559ba43ec01388065bad
EduardoAmaro <dudaamarorosa@gmail.com>
2019-05-28 02:04:55 -0300

    Implementação das políticas de substituição

--- src/cache_simulator/Cache.java
+++ src/cache_simulator/Cache.java
@@ -1,38 +1,51 @@
 package cache_simulator;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
 
 public class Cache {
 
     private Integer noffset, nindice, ntag;
+    private Integer assoc, nsets, bsize, polSubs;
     private Integer hit, miss_conflito, miss_compulsorio, miss_capacidade, miss_total;
 
     private ArrayList<ArrayList<Bloco>> conjuntos;
+    private ArrayList<LinkedList<Integer>> ordemSubstituicao;
 
-    public Cache(Integer nsets, Integer assoc, Integer bsize) {
+    public Cache(Integer nsets, Integer assoc, Integer bsize, Integer polSubs) {
+        this.assoc = assoc;
+        this.bsize = bsize;
+        this.nsets = nsets;
+        this.polSubs = polSubs;
+
         this.noffset = (int) (Math.log(bsize) / Math.log(2));
         this.nindice = (int) (Math.log(nsets) / Math.log(2));
         this.ntag = 32 - this.noffset - this.nindice;
+
+        this.hit = 0;
+        this.miss_conflito = 0;
+        this.miss_compulsorio = 0;
+        this.miss_capacidade = 0;
+        this.miss_total = 0;
+
         this.conjuntos = new ArrayList<>(assoc);
+        this.ordemSubstituicao = new ArrayList<>(nsets);
 
         for (int i = 0; i < assoc; i++) {
             ArrayList<Bloco> blocos = new ArrayList<>();
-            for (int j = 0; j < nsets / assoc; j++) {
+            for (int j = 0; j < nsets; j++) {
                 Bloco bloco = new Bloco();
                 blocos.add(bloco);
             }
             this.conjuntos.add(blocos);
         }
 
-        /*
-        for(int i=0;i<assoc;i++){
-            System.out.print("[");
-            for(int j=0;j<nsets/assoc;j++){    
-                System.out.print(j + ",");
-            }
-            System.out.print("]\n");
+        for (int i = 0; i < nsets; i++) {
+            LinkedList<Integer> listaSubs = new LinkedList<>();
+            this.ordemSubstituicao.add(listaSubs);
         }
-         */
+
     }
 
     /**
@@ -78,28 +91,46 @@ public class Cache {
     }
 
     public void acessaCache(String endereco) {
+        boolean flagMiss = false;
         for (int i = 0; i < this.conjuntos.size(); i++) {
-            if (this.conjuntos.get(i).get(this.getIntIndice(endereco)).getValidade() == 0) {//sem informação no bloco miss compulsório
-                this.escreveCache(i, this.getIntIndice(endereco), this.getSplitTag(endereco));
+            if (this.conjuntos.get(i).get(this.getIntIndice(this.getSplitIndice(endereco))).getValidade() == 0) {//sem informação no bloco miss compulsório
+                this.escreveCache(i, this.getIntIndice(this.getSplitIndice(endereco)), this.getSplitTag(endereco));
+                this.ordemSubstituicao.get(this.getIntIndice(this.getSplitIndice(endereco))).addLast(i);
                 this.miss_compulsorio++;
                 this.miss_total++;
+                flagMiss = false;
                 return;
-            } else if (this.conjuntos.get(i).get(this.getIntIndice(endereco)).getValidade() == 1) {//encontrou informação no bloco
-                if (this.conjuntos.get(i).get(this.getIntIndice(endereco)).getTag() == this.getSplitTag(endereco)) {//verifica tag
+            } else if (this.conjuntos.get(i).get(this.getIntIndice(this.getSplitIndice(endereco))).getValidade() == 1) {//encontrou informação no bloco
+                if (this.conjuntos.get(i).get(this.getIntIndice(this.getSplitIndice(endereco))).getTag() == this.getSplitTag(endereco)) {//verifica tag
+                    flagMiss = false;
                     this.hit++;
+                    if (polSubs == 2) {
+                        int lru = ordemSubstituicao.get(this.getIntIndice(this.getSplitIndice(endereco))).removeFirst();
+                        this.ordemSubstituicao.get(this.getIntIndice(this.getSplitIndice(endereco))).addLast(lru);
+                    }
                     return;
                 } else {
-                    
+                    flagMiss = true;
                 }
-            } else if (this.conjuntos.get(i).get(this.getIntIndice(endereco)).getValidade() == -1) {
-                this.miss_compulsorio++;
-                this.miss_total++;
-                this.escreveCache(i, this.getIntIndice(endereco), this.getSplitTag(endereco));
-                return;
-            } else {
-
             }
+        }
 
+        if (flagMiss == true) {
+            if (polSubs == 0) {//random
+                Random gerador = new Random();
+                int gerado = gerador.nextInt(this.conjuntos.size());
+                
+                //System.out.println(gerado);
+                //System.out.println(this.getIntIndice(this.getSplitIndice(endereco)));
+                
+                this.escreveCache(gerado, this.getIntIndice(this.getSplitIndice(endereco)), this.getSplitTag(endereco));
+                this.miss_conflito++;
+                this.miss_total++;
+            } else if (polSubs == 1 || polSubs == 2) {//fifo
+                Integer sub = ordemSubstituicao.get(this.getIntIndice(this.getSplitIndice(endereco))).removeFirst();
+                this.escreveCache(sub, this.getIntIndice(this.getSplitIndice(endereco)), this.getSplitTag(endereco));
+                this.ordemSubstituicao.get(this.getIntIndice(this.getSplitIndice(endereco))).addLast(sub);
+            }
         }
     }
 
@@ -110,12 +141,23 @@ public class Cache {
 
     public boolean busca(String endereco) {
         for (int i = 0; i < this.conjuntos.size(); i++) {
-            if (this.conjuntos.get(i).get(this.getIntIndice(endereco)).getTag() == this.getSplitTag(endereco)
-                    & this.conjuntos.get(i).get(this.getIntIndice(endereco)).getValidade() == 1) {
+            if (this.conjuntos.get(i).get(this.getIntIndice(this.getSplitIndice(endereco))).getTag().equals(this.getSplitTag(endereco))
+                    && this.conjuntos.get(i).get(this.getIntIndice(this.getSplitIndice(endereco))).getValidade() == 1) {
                 return true;
             }
         }
         return false;
+    }
+
+    public Integer estaCheia() {//-1 lista cheia || índice de primeiro local vazio
+        for (int i = 0; i < this.assoc; i++) {
+            for (int j = 0; j < this.nsets; j++) {
+                if (this.conjuntos.get(i).get(j).getValidade() == 0) {
+                    return j;
+                }
+            }
+        }
+        return -1;
     }
 
     public String getSplitTag(String endereco) {
@@ -128,5 +170,18 @@ public class Cache {
 
     private Integer getIntIndice(String indice) {
         return Integer.parseInt(indice, 2);
+    }
+
+    @Override
+    public String toString() {
+        String saida = "";
+        for (int i = 0; i < this.assoc; i++) {
+            System.out.println("----------------------------------------------------------");
+            for (int j = 0; j < this.nsets; j++) {
+                System.out.println(this.conjuntos.get(i).get(j).getTag() + "|");
+            }
+        }
+
+        return saida;
     }
 }
--- src/cache_simulator/Cache_simulator.java
+++ src/cache_simulator/Cache_simulator.java
@@ -6,6 +6,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
+//faltando:
+//implementação da escrita em posição seguinte vazia - totalmente associativa - miss
+//retorno e controle dos misses e hits
+//segundo nível de cache (se der)
+
 public class Cache_simulator {
 
     public static void main(String[] args) {
@@ -18,6 +23,14 @@ public class Cache_simulator {
         Integer nsets = Integer.parseInt(config.split(":")[0]);
         Integer bsize = Integer.parseInt(config.split(":")[1]);
         Integer assoc = Integer.parseInt(config.split(":")[2]);
+        Integer polSubs = Integer.parseInt(config.split(":")[3]);//0 - random 1 - fifo 2 - lru
+
+        if (nsets == 0 || bsize == 0 || assoc == 0) {//default
+            nsets = 256;
+            bsize = 4;
+            assoc = 1;
+            polSubs = 0;
+        }
 
         InputStream input;
         try {
@@ -34,30 +47,36 @@ public class Cache_simulator {
                 }
                 enderecos.add(endereco);
             }
-            
+
             in.close();
 
         } catch (IOException e) {
             //
         }
-        
+
         for (int i = 0; i < enderecos.size(); i++) {
             System.out.println(enderecos.get(i));
         }
-        System.out.println(enderecos.size());
-        
-        Cache cacheL1 = new Cache(nsets,assoc,bsize);
-        
+
+        //System.out.println(enderecos.size());
+        Cache cacheL1 = new Cache(nsets, assoc, bsize, polSubs);
+
         //teste splits e tamanhos
         System.out.println("\n\n");
         System.out.println(cacheL1.getNtag() + " " + cacheL1.getNindice() + " " + cacheL1.getNoffset());
         System.out.println(enderecos.get(0));
-        System.out.print(cacheL1.getSplitTag(enderecos.get(0))+"|");
+        System.out.print(cacheL1.getSplitTag(enderecos.get(0)) + "|");
         System.out.println(cacheL1.getSplitIndice(enderecos.get(0)));
-        
+
+        for (int i = 0; i < enderecos.size(); i++) {
+            cacheL1.acessaCache(enderecos.get(i));
+        }
+
         System.out.println(cacheL1.busca(enderecos.get(0)));
-        
-     
+
+        System.out.println("--------------------------------------");
+        System.out.println(cacheL1.toString());
+
     }
 
     public static String preencheBinario(String endereco) {
@@ -67,11 +86,11 @@ public class Cache_simulator {
         return endereco;
     }
 
-/*
+    /*
     cache_simulator<nsets_L1>:<bsize_L1>:<assoc_L1> arquivo_de_entrada
     
     rodar na pasta src/
     javac cache_simulator/Cache_simulator.java
     java cache_simulator.Cache_simulator 256:64:4 arqBinario1.dat
-*/
+     */
 }
